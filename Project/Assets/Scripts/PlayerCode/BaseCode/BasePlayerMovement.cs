c1b7b915e250a54a4d698dbe4b5d582bdf4c2f16
AnimeThing <animething984@gmail.com>
2025-09-09 12:04:27 +0100

    Build Folder + Movement Changes
    old code has an issue where jump wouldn't work, compacted the code a bit slight making the line total a little less (this is temporary)
    Added build folder so when the project is completed, we can provide a link to the github, which will now include the project files and the build (could create a folder for assets)

--- Project/Assets/Scripts/PlayerCode/BaseCode/BasePlayerMovement.cs
+++ Project/Assets/Scripts/PlayerCode/BaseCode/BasePlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,23 +6,26 @@ namespace PlayerCode.BaseCode {
     [RequireComponent(typeof(PlayerInput))]
     public abstract class BasePlayerMovement : MonoBehaviour {
         #region Variables
-        public virtual float walkSpeed => 3f;
-        public virtual float jumpHeight => 5f;
-        public virtual LayerMask groundMask => 3; //change default to ground layer
 
-        //tempt serialize field
+        //default values that can be changed later
+        public virtual float walkSpeed => 3f;
+        public virtual float jumpHeight => 2f;
+        public LayerMask groundMask = 3; //change default to ground layer
+
+        //stores the player inputs into variables
         private Vector2 _moveInput;
         private bool _jumpButtonDown;
 
         //references
         protected Rigidbody rb;
-        
+
         #endregion
-        
+
         #region Unity Methods
+
         private void Start() {
-            rb =  GetComponent<Rigidbody>();
-            
+            rb = GetComponent<Rigidbody>();
+
             rb.constraints = RigidbodyConstraints.FreezePositionZ;
             rb.constraints = RigidbodyConstraints.FreezeRotation;
         }
@@ -42,6 +44,7 @@ namespace PlayerCode.BaseCode {
         #endregion
 
         #region Input Methods
+
         public void MoveInput(InputAction.CallbackContext c) {
             _moveInput = c.ReadValue<Vector2>();
         }
@@ -49,8 +52,9 @@ namespace PlayerCode.BaseCode {
         public void HandleJumpInput(InputAction.CallbackContext c) {
             _jumpButtonDown = c.performed;
         }
+
         #endregion
-        
+
         #region Movement Methods
 
         protected virtual void HandleMovement() {
@@ -60,48 +64,38 @@ namespace PlayerCode.BaseCode {
 
         protected virtual void HandleJump() {
             if (!(_jumpButtonDown && isGrounded)) return;
-            
+
             rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
         }
-        
+
         #endregion
-        
+
         #region Combat Methods
 
-        private void CombatManager() {
-            
-        }
+        private void CombatManager() { }
+
+        protected virtual void LightAttack() { }
+
+        protected virtual void HeavyAttack() { }
+
+        protected virtual void ArialAttack() { }
+
+        protected virtual void UpwardsAttack() { }
+
+        protected virtual void DownwardsAttack() { }
+
+        protected virtual void Block() { }
         
-        protected virtual void LightAttack() {
-            
-        }
-
-        protected virtual void HeavyAttack() {
-            
-        }
-
-        protected virtual void ArialAttack() {
-            
-        }
-
-        protected virtual void UpwardsAttack() {
-            
-        }
-
-        protected virtual void DownwardsAttack() {
-            
-        }
-
-        protected virtual void Block() {
-            
-        }
+        protected virtual void Ability() { }
         
+        protected virtual void Ultimate() { }
+
         #endregion
-        
+
         #region Check Methods
 
-        protected bool isGrounded => Physics.Raycast(transform.position, Vector3.down, 1.2f);
-        
+        protected bool isGrounded => Physics.Raycast(transform.position, Vector3.down, 1.2f, groundMask);
+
         #endregion
     }
 }
